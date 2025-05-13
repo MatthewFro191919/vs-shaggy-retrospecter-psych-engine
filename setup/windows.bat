3f15a8ff95896d4e78873cffa9d244c5a5dc26d3
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-13 15:17:19 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -28,6 +28,7 @@ echo This might take a few moments depending on your internet speed.
           haxelib git faxe https://github.com/uhrobots/faxe
           haxelib git polymod https://github.com/larsiusprime/polymod.git
           haxelib install hxcpp-debug-server
+          haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
           haxelib git SScript https://github.com/AltronMaxX/SScript.git
 echo Finished!
 pause
